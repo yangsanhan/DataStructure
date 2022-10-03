9973dfd694be1ac0f01b19f08ea5feab21f53ff6
YangYang <1826143821@qq.com>
2022-10-03 13:56:35 +0800

    “替换_count为_top”

--- SeqStack/SeqStack.cs
+++ SeqStack/SeqStack.cs
@@ -3,22 +3,22 @@
     public class SeqStack<T>
     {
         private T[] _data;
-        private int _count;
+        private int _top;
 
         public SeqStack(int capacity = 4)
         {
             _data = new T[capacity];
-            _count = 0;
+            _top = 0;
         }
 
         public int Count => _count;
 
         public void Push(T data)
         {
-            if (_count == _data.Length)
+            if (_top == _data.Length)
                 EnSureCapacity(_data.Length * 2);
 
-            _data[_count++] = data;
+            _data[_top++] = data;
         }
 
         public T Peek()
@@ -26,7 +26,7 @@
             if (IsEmpty())
                 return default(T);
             else
-                return _data[_count - 1];
+                return _data[_top - 1];
         }
 
         public T Pop()
@@ -35,8 +35,8 @@
                 return default(T);
             else
             {
-                T popValue = _data[--_count];
-                if (_count > 0 && _count < _data.Length / 4)
+                T popValue = _data[--_top];
+                if (_top > 0 && _top < _data.Length / 4)
                     EnSureCapacity(_data.Length / 2);
 
                 
@@ -47,13 +47,13 @@
 
         public bool IsEmpty()
         {
-            return _count == 0;
+            return _top == 0;
         }
 
         public void EnSureCapacity(int newCapacity)
         {
             T[] newData = new T[newCapacity];        
-            for (int i = 0; i < _count; i++)
+            for (int i = 0; i < _top; i++)
                 newData[i] = _data[i];
 
             _data = newData;
