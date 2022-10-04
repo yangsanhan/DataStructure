6bba7ea6abdb286c4c7fad862435cc65548f7920
YangYang <1826143821@qq.com>
2022-10-04 11:07:45 +0800

    整理文件夹结构 替换栈实现

---
+++ Stack/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public interface IStack<T>
+    {
+        int Count { get; }
+        bool IsEmpty();
+        void Push(T item);
+        T Peek();
+        T Pop();
+    }
+}
